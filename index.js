7dcfde3dc9eea4ad4764a611a32a406515d02e84
Kevin McGuire <kevin@givegab.com>
2020-06-08 11:51:41 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const screenshot = require('url-to-screenshot')
+const dotenv = require('dotenv').config()
+
+var whitelist = [
+  `http://localhost:${process.env.INCOMING_PORT}`,
+  `https://localhost:${process.env.INCOMING_PORT}`,
+  `https://${process.env.LIVE_URL}/`,
+];
+
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.get('/image', cors(corsOptions), (req, res, next)=>{
+  let url = req.query.url
+  new screenshot(url)
+    .sslProtocol('any')
+    .width(1600)
+    .height(1200)
+    .clip()
+    .capture()
+    .then(img =>{
+      imgdata = img.toString('base64')
+      res.setHeader('Content-Type', 'application/json')
+      res.send(JSON.stringify({image: imgdata}))
+    })
+})
+
+var port = process.env.PORT || 5000;
+
+app.listen(port);
+console.log("server started " + port);
